8bbbb6e2ecd38b8d3c029643edcd5527edf92abe
Mahendra Saini <masaini@janes-MacBook-Pro.local>
2022-07-25 23:37:01 +0530

    code clean up
    Signed-off-by: Mahendra Saini <masaini@janes-MacBook-Pro.local>

--- src/main/java/com/anakin/controllers/UserController.java
+++ src/main/java/com/anakin/controllers/UserController.java
@@ -1,21 +1,11 @@
 package com.anakin.controllers;
 
-import com.anakin.models.JwtRequest;
-import com.anakin.models.JwtResponse;
 import com.anakin.payloads.requests.UserLoginRequest;
 import com.anakin.payloads.requests.UserSignUpRequest;
 import com.anakin.payloads.responses.UserLoginResponse;
 import com.anakin.payloads.responses.UserSignUpResponse;
-import com.anakin.security.JwtTokenUtil;
-import com.anakin.security.JwtUserDetailsService;
 import com.anakin.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
